1b5bcd72d3010271a4b35cc3d4ef3dc9100defb2
Raiquawn Malloy <ray@lsadv.com>
2017-05-30 20:25:21 -0400

    Authentication Branch Initial Commit

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+var express      = require('express');
+var path         = require('path');
+var favicon      = require('serve-favicon');
+var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var bodyParser   = require('body-parser');
+var passport     = require('passport');
+var session      = require('express-session');
+var dateFormat   = require('dateformat');
 
+
+// Routers
 var index = require('./routes/index');
 var users = require('./routes/users');
+var signup = require('./routes/signup');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,9 +28,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/signup', signup);
+
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
