ae37721a390fdc21ca300de8fc2d641054b3eaed
Raiquawn Malloy <ray@lsadv.com>
2017-06-04 10:31:43 -0400

    changes to index.js, and added new view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+var express       = require('express');
+var router        = express.Router();
+var multer        = require('multer');
+var sharp         = require('sharp');
+var models        = require('../models/index');
+var photo          = models.photo;
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/new', function(request, response) {
+	if (true)
+		response.render('photoupload/new', {
+			post: {}
+		});
+	else
+		response.redirect('/user/log-in');
 });
 
 module.exports = router;
