5421afa7869514aa0559ac67f8e668ca26693154
Raiquawn Malloy <ray@lsadv.com>
2017-06-04 12:48:26 -0400

    in progress photo upload feature.

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,14 @@ var router        = express.Router();
 var multer        = require('multer');
 var sharp         = require('sharp');
 var models        = require('../models/index');
-var photo          = models.photo;
+var photo         = models.photo;
+var uploadHandler = multer();
+
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index/index', { title: 'Express' });
 });
 
 router.get('/new', function(request, response) {
@@ -19,5 +21,48 @@ router.get('/new', function(request, response) {
 	else
 		response.redirect('/user/log-in');
 });
+
+router.post('/index', uploadHandler.single('image'), function(request, response) {
+	photo.create({
+		title:         request.body.title,
+		// body:          request.body.body,
+		slug:          request.body.slug,
+		userId:        request.user.id
+	}).then(function(photo) {
+		sharp(request.file.buffer)
+		.resize(300, 300)
+		.max()
+		.withoutEnlargement()
+		.toBuffer()
+    response.render('photoupload/new', {
+			// post:   request.body,
+		});
+		// .then(function(thumbnail) {
+		// 	s3.upload({
+		// 		Bucket:     'instagram',
+		// 		Key:        `photo/${photo.id}`,
+		// 		Body:        request.file.buffer,
+		// 		ACL:        'public-read',
+		// 		ContentType: request.file.mimetype
+		// 	}, function(error, data) {
+		// 		s3.upload({
+		// 			Bucket:     'instagram',
+		// 			Key:        `photo/${photo.id}-thumbnail`,
+		// 			Body:        thumbnail,
+		// 			ACL:        'public-read',
+		// 			ContentType: request.file.mimetype
+		// 		}, function(error, data) {
+		// 			response.redirect(photo.url);
+		// 		});
+		// 	});
+		});
+	}).catch(function(error) {
+		response.render('photoupload/new', {
+			// post:   request.body,
+			errors: error.error
+		});
+	});
+});
+
 
 module.exports = router;
