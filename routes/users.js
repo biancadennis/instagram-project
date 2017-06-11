3809670c81f8c86f4965348d3d4a255188c5a43a
Bianca Dennis <bianca.dennis@caa.columbia.edu>
2017-06-11 15:09:10 -0400

    commit

--- routes/users.js
+++ routes/users.js
@@ -201,7 +201,17 @@ router.get('/photo/:id', function(request, response) {
         });
     });
 });
-
+router.get('/photo/:id', function(request, response) {
+    Photo.findById(request.params.id, {
+			include: [
+				Comment
+			]
+		}).then(function(photo) {
+        response.render('photoupload/show', {
+            photo: photo
+        });
+    });
+});
 router.post('/photo/:id/comments', function(request, response) {
 	Photo.findById(request.params.id).then(function(photo) {
 		photo.createComment({
